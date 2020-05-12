bbfa0d78a2ef62212e2d0a95a1ab1b515d138665
victorBOY <32583593+vWernay@users.noreply.github.com>
2020-05-12 19:22:27 -0300

    CRIAÇÃO DO FXMANIFEST.LUA

---
+++ fxmanifest.lua
@@ -0,0 +1,15 @@
+fx_version 'bodacious'
+game 'gta5'
+
+
+author 'victorBOY'
+description '(BETA) Perder itens ao cair na água.'
+version '1.0.0'
+
+client_scripts {
+	'client/main.lua'
+}
+
+server_scripts {
+	'server/main.lua'
+}
