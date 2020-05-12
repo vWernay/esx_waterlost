6cb1e07de551b69af04ceeca1e5ab1c98cc88705
victorBOY <32583593+vWernay@users.noreply.github.com>
2020-05-12 19:19:34 -0300

    CRIAÇÃO DO SQL

---
+++ esx_perderitens.sql
@@ -0,0 +1,12 @@
+USE `essentialmode`;
+
+INSERT INTO `items` (`name`, `label`) VALUES
+	('gpsqueimado', 'GPS Queimado'),
+	('celqueimado', 'Celular queimado'),
+	('vapequeimado', 'Vape queimado'),
+	('radioqueimado', 'Radio queimado'),
+	('sandwich', 'Macka'),
+	('rose', 'Röd ros'),
+	('bulletproof', 'Skottsäker vest'),
+	('redgull', 'Redgull')
+;
