c9b7dfeeae308565e433f330812317732d168bcc
victorBOY <32583593+vWernay@users.noreply.github.com>
2020-05-12 00:00:00 -0300

    Create main.lua

---
+++ main.lua
@@ -0,0 +1,24 @@
+------- CARREGAMENTO PADRÃO DA ESX --------
+
+ESX = nil
+
+Citizen.CreateThread(function()
+	while ESX == nil do
+		TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
+		Citizen.Wait(0)
+	end
+end)
+
+-------------------------------------------
+
+--Beta
+
+Citizen.CreateThread(function()
+    while true do
+		Citizen.Wait(60)
+		if IsPedSwimming(PlayerPedId(), false) then
+			--print('O Player está nadando!')
+			TriggerServerEvent('esx_perderitens:perderTudo')
+		end
+	end	
+end)
