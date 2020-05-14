979a2f06997bc13f9b1b6897de0e5f5a68faa27b
victorBOY <32583593+vWernay@users.noreply.github.com>
2020-05-14 13:38:47 -0300

    ATUALIZAÇÃO SQL

--- esx_perderitens.sql
+++ esx_perderitens.sql
@@ -4,9 +4,5 @@ INSERT INTO `items` (`name`, `label`) VALUES
 	('gpsqueimado', 'GPS Queimado'),
 	('celqueimado', 'Celular queimado'),
 	('vapequeimado', 'Vape queimado'),
-	('radioqueimado', 'Radio queimado'),
-	('sandwich', 'Macka'),
-	('rose', 'Röd ros'),
-	('bulletproof', 'Skottsäker vest'),
-	('redgull', 'Redgull')
+	('radioqueimado', 'Radio queimado')
 ;
