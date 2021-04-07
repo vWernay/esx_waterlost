88e7676fde7ed81b308f5ccd693423b5c9195b66
victorBOY <32583593+vWernay@users.noreply.github.com>
2021-04-07 18:19:47 -0300

    Update main.lua

--- client/main.lua
+++ client/main.lua
@@ -1,22 +1,8 @@
------------------------ESX, NÃO TOQUE!----------------------------------
-ESX = nil
-
-Citizen.CreateThread(function()
-	while ESX == nil do
-		TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
-		Citizen.Wait(0)
-	end
-end)
--------------------------------------------------------------------------
-
-
--- Beta
-
 Citizen.CreateThread(function()
     while true do
 		Citizen.Wait(2500)
 		if IsPedSwimming(PlayerPedId(), false) then
-			TriggerServerEvent('esx_perderitens:perderTudo')
+			TriggerServerEvent('esx_perderitens:caiuNaAgua')
 		end
 	end	
 end)
