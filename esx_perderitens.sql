9f108aebb17b9db3e08e8139087ed758f7110145
victorBOY <32583593+vWernay@users.noreply.github.com>
2021-04-07 18:26:03 -0300

    Delete esx_perderitens.sql

--- esx_perderitens.sql
+++
@@ -1,8 +0,0 @@
-USE `essentialmode`;
-
-INSERT INTO `items` (`name`, `label`) VALUES
-	('gpsqueimado', 'GPS Queimado'),
-	('celqueimado', 'Celular queimado'),
-	('vapequeimado', 'Vape queimado'),
-	('radioqueimado', 'Radio queimado')
-;
