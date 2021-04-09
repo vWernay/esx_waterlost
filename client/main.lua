cf53a04453982d4c5be0e05117c67e23a5a949c1
victorBOY <32583593+vWernay@users.noreply.github.com>
2021-04-09 00:24:57 -0300

    Update main.lua

--- client/main.lua
+++ client/main.lua
@@ -2,7 +2,7 @@ Citizen.CreateThread(function()
     while true do
 		Citizen.Wait(2500)
 		if IsPedSwimming(PlayerPedId(), false) then
-			TriggerServerEvent('esx_perderitens:caiuNaAgua')
+			TriggerServerEvent('esx_waterlost:caiuNaAgua')
 		end
 	end	
 end)
