483424e481fc9b237e1b89c14abfaa0896283d58
victorBOY <32583593+vWernay@users.noreply.github.com>
2021-05-11 00:17:45 -0300

    Import MySQL

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -3,12 +3,13 @@ game 'gta5'
 
 author 'victorBOY'
 description 'Lose items on falling into Water'
-version '1.1'
+version '1.1.2'
 
 lua54 'yes'
 client_script 'client/*.lua'
 
 server_scripts {
+	'@mysql-async/lib/MySQL.lua',
 	'@es_extended/locale.lua',
 	'locales/*.lua',
 	'config.lua',
